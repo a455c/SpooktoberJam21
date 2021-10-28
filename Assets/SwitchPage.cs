77898e30c8c91e3a0cd2bdc308e71d95ed0240f9
lunarcards <isaacupcroft@gmail.com>
2021-10-28 20:21:27 +0100

    Main menu updated with new info

---
+++ Assets/SwitchPage.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class SwitchPage : MonoBehaviour
+{
+    public TMP_Text storyText;
+    public TMP_Text htpText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void NextPage()
+    {
+        if (storyText.gameObject.activeSelf)
+        {
+            storyText.gameObject.SetActive(false);
+            htpText.gameObject.SetActive(true);
+        }
+        else if (htpText.gameObject.activeSelf)
+        {
+            storyText.gameObject.SetActive(true);
+            htpText.gameObject.SetActive(false);
+        }
+    }
+}
