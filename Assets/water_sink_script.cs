90bb1d7e6c5c38a89f957d5823ccebfee1657c23
Ben Upcroft <upcroft@gmail.com>
2021-10-30 19:16:34 +0100

    Added Level 5 with water obstacle

---
+++ Assets/water_sink_script.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class water_sink_script : MonoBehaviour
+{
+    public AudioSource sinking;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("Run into river");
+            sinking.Play();
+            Destroy(collision.gameObject);
+        }
+    }
+}
